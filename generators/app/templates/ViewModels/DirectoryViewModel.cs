2592c3073ce76d8874729c98d5114909f1670984
Sidoine De Wispelaere <sdewispelaere@talentsoft.com>
2015-12-10 15:21:59 +0100

    #1 Remove unnecessary files

--- generators/app/templates/ViewModels/DirectoryViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace <%= name %>.ViewModels
-{
-    public class DirectoryViewModel
-    {
-        public string Path { get; set; }
-        public string Name { get; set; }
-    }
-}
--- generators/app/templates/ViewModels/PhotoViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace <%= name %>.ViewModels
-{
-    public class PhotoViewModel
-    {
-        public string Path { get; set; }
-        public string Name { get; set; }
-    }
-}
